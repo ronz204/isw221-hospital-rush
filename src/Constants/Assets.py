4fc6ebfab9e895d536b5083a03dee5f9dc45f62c
roney ruiz <roneyruizrojas@gmail.com>
2025-07-28 21:59:00 -0600

    feat: create button assets enum

--- src/Constants/Assets.py
+++ src/Constants/Assets.py
@@ -4,3 +4,8 @@ class Scenarios(Enum):
   HOME = "Scenarios/HomeScenario.png"
   ROOM = "Scenarios/RoomScenario.png"
   HISTORY = "Scenarios/HistoryScenario.png"
+
+class Buttons(Enum):
+  EXIT = "Buttons/ExitButton.png"
+  START = "Buttons/StartButton.png"
+  HISTORY = "Buttons/HistoryButton.png"
