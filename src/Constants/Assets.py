20d35aa030ae82008e440a6e2a06fcbd575a5b8a
roney ruiz <roneyruizrojas@gmail.com>
2025-07-29 21:01:38 -0600

    chore: update scenario assets

--- src/Constants/Assets.py
+++ src/Constants/Assets.py
@@ -1,11 +1,12 @@
 from enum import Enum
 
-class Scenarios(Enum):
+class Scenario(Enum):
   HOME = "Scenarios/HomeScenario.png"
   ROOM = "Scenarios/RoomScenario.png"
+  MENU = "Scenarios/MenuScenario.png"
   HISTORY = "Scenarios/HistoryScenario.png"
 
-class Buttons(Enum):
+class Button(Enum):
   EXIT = "Buttons/ExitButton.png"
   START = "Buttons/StartButton.png"
   HISTORY = "Buttons/HistoryButton.png"
