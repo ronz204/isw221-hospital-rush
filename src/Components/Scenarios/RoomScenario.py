49d399e47b907415a17659178a94688aa8a4b353
roney ruiz <roneyruizrojas@gmail.com>
2025-07-31 20:51:56 -0600

    feat: create and placed doctor characters

--- src/Components/Scenarios/RoomScenario.py
+++ src/Components/Scenarios/RoomScenario.py
@@ -1,14 +1,27 @@
-from src.Constants.Assets import Scenario
+from pygame.sprite import Group
+from src.Models.Coord import Coord
+from src.Constants.Assets import Scenario, Character
 from src.Components.Scenarios.BaseScenario import BaseScenario
+from src.Components.Characters.DoctorCharacter import DoctorCharacter
+from src.Components.Characters.TriageCharacter import TriageCharacter
+from src.Components.Characters.PatientCharacter import PatientCharacter
 
 class RoomScenario(BaseScenario):
   BACKGROUND = Scenario.ROOM
 
   def __init__(self):
     super().__init__()
+    self.characters = Group()
+
+    self.characters.add(
+      DoctorCharacter(Coord(30, 30), Character.DOCTOR1),
+      DoctorCharacter(Coord(30, 120), Character.DOCTOR2),
+      DoctorCharacter(Coord(30, 210), Character.DOCTOR3),
+      DoctorCharacter(Coord(30, 300), Character.DOCTOR4),)
 
   def listen(self, event) -> None:
     pass
 
   def draw(self, screen) -> None:
     super().draw(screen)
+    self.characters.draw(screen)
