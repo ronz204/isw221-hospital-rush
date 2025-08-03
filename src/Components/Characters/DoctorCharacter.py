7b7fcd8e66c5071842aa2ea2be9621bfd24e0dd4
roney ruiz <roneyruizrojas@gmail.com>
2025-08-03 10:48:22 -0600

    feat: implement draggable mixin in specific characters

--- src/Components/Characters/DoctorCharacter.py
+++ src/Components/Characters/DoctorCharacter.py
@@ -1,8 +1,13 @@
 from src.Models.Size import Size
 from src.Models.Coord import Coord
 from src.Constants.Assets import Character
+from src.Components.DragDrop.Draggable import Draggable
 from src.Components.Characters.BaseCharacter import BaseCharacter
 
-class DoctorCharacter(BaseCharacter):
+class DoctorCharacter(BaseCharacter, Draggable):
   def __init__(self, coords: Coord, image: Character):
     super().__init__(coords=coords, size=Size(55, 55), image=image)
+    Draggable.__init__(self)
+
+  def listen(self, event) -> None:
+    self.listen_drag(event)
