5a46e4df5da426e339e3c853dcdf2bdcc30c67c2
roney ruiz <roneyruizrojas@gmail.com>
2025-08-03 10:48:39 -0600

    feat: testing draggable characters

--- src/Components/Scenarios/RoomScenario.py
+++ src/Components/Scenarios/RoomScenario.py
@@ -17,11 +17,21 @@ class RoomScenario(BaseScenario):
     self.stretchers = Group()
     self.characters = Group()
 
+    self.characters.add(
+      DoctorCharacter(Coord(430, 160), Character.DOCTOR1),
+      DoctorCharacter(Coord(500, 180), Character.DOCTOR2),
+      
+      PatientCharacter(Coord(530, 330), Character.PATIENT1),
+      PatientCharacter(Coord(560, 380), Character.PATIENT2),)
+
     self.stretchers.add(
       Stretcher(Coord(290, 190)),
       Stretcher(Coord(110, 300)),)
 
   def listen(self, event) -> None:
+    for character in self.characters:
+      character.listen(event)
+
     for stretcher in self.stretchers:
       stretcher.listen(event)
 
