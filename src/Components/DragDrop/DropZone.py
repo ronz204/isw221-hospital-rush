c4b2331de035161cf8a932839ada6c16a04b015e
roney ruiz <roneyruizrojas@gmail.com>
2025-08-03 15:27:53 -0600

    refac: convert dropzone atomic

--- src/Components/DragDrop/DropZone.py
+++ src/Components/DragDrop/DropZone.py
@@ -2,51 +2,40 @@ import pygame
 from typing import List
 from pygame.event import Event
 from src.Models.Size import Size
-from pygame import draw, Surface, Rect
+from pygame import draw, Surface
 from src.Components.Component import Component
 from src.Components.DragDrop.Draggable import Draggable
 
-class DropZone:
-  def __init__(self, rect: Rect):
-    self.parent_rect = rect
-    self.slot_count: int = 2
-    self.slot_padding: int = 10
-    self.slot_size: Size = Size(38, 38)
-    self.objects_inside: List[Component] = []
+class DropZone(Component):
+  def __init__(self, coords, size: Size = Size(38, 38), padding: int = 10):
+    super().__init__(coords=coords, size=size)
+
+    self.padding = padding
+    self.object_inside: Component = None
 
   @property
-  def can_accept(self):
-    return len(self.objects_inside) < self.slot_count
+  def position(self):
+    return (self.rect.x, self.rect.y)
 
-  @property
-  def slot_positions(self):
-    y = self.parent_rect.y + (self.parent_rect.height - self.slot_size.height) // 2
-    return [
-      (self.parent_rect.x + self.slot_padding, y),
-      (self.parent_rect.x + self.parent_rect.width - self.slot_padding - self.slot_size.width, y)]
-  
   def is_inside(self, component: Component) -> bool:
-    return component.rect.colliderect(self.parent_rect)
+    return component.rect.colliderect(self.rect)
   
-  def update_positions(self):
-    for index, drag in enumerate(self.objects_inside):
-      if index < len(self.slot_positions):
-        drag.rect.topleft = self.slot_positions[index]
+  def update(self) -> None:
+    if (self.object_inside):
+      if not self.is_inside(self.object_inside):
+        self.object_inside = None
+      else:
+        self.object_inside.rect.topleft = self.position
 
-  def update_slots(self):
-    self.objects_inside = [obj for obj in self.objects_inside if self.is_inside(obj)]
-    self.update_positions()
-  
   def listen(self, event: Event, draggables: List[Draggable]) -> None:
-    if (event.type == pygame.MOUSEBUTTONUP and event.button == 1 and self.can_accept):
+    if (event.type == pygame.MOUSEBUTTONUP and event.button == 1 and self.object_inside is None):
       for drag in draggables:
-        if not drag.is_dragging and drag not in self.objects_inside and self.is_inside(drag):
-          self.objects_inside.append(drag)
-          self.update_positions()
+        if not drag.is_dragging and self.is_inside(drag):
+          self.object_inside = drag
+          self.object_inside.rect.topleft = self.position
           break
-  
+
   def draw(self, screen: Surface) -> None:
-    for i, pos in enumerate(self.slot_positions):
-      color = (100, 200, 100) if i < len(self.objects_inside) else (255, 255, 255)
-      draw.rect(screen, color, (*pos, *self.slot_size.as_tuple()), border_radius=6)
-      draw.rect(screen, (120, 120, 120), (*pos, *self.slot_size.as_tuple()), width=3, border_radius=6)
+    color = (100, 200, 100) if self.object_inside else (255, 255, 255)
+    draw.rect(screen, color, self.rect, border_radius=6)
+    draw.rect(screen, (120, 120, 120), self.rect, width=3, border_radius=6)
--- src/Components/Equipment/Stretcher.py
+++ src/Components/Equipment/Stretcher.py
@@ -12,12 +12,20 @@ class Stretcher(BaseEquipment):
     super().__init__(coords=coords, size=Size(150, 90))
     self.image = AssetHelper.load_image(Equipment.STRETCHER.value, self.size.as_tuple())
 
-    self.dropzone = DropZone(self.rect)
+    left_zone_coords = Coord(self.rect.x + 10, self.rect.y + (self.rect.height - 38) // 2)
+    right_zone_coords = Coord(self.rect.x + self.rect.width - 10 - 38, self.rect.y + (self.rect.height - 38) // 2)
+
+    self.dropzones = [
+      DropZone(coords=left_zone_coords),
+      DropZone(coords=right_zone_coords),]
 
   def listen(self, event, draggables: List[Draggable]) -> None:
-    self.dropzone.listen(event, draggables)
-    self.dropzone.update_slots()
+    for zone in self.dropzones:
+      zone.listen(event, draggables)
+      zone.update()
 
   def draw(self, screen) -> None:
-    self.dropzone.parent_rect = self.rect
-    self.dropzone.draw(screen)
+    for zone in self.dropzones:
+      zone.rect.x = self.rect.x + (10 if zone is self.dropzones[0] else self.rect.width - 10 - 38)
+      zone.rect.y = self.rect.y + (self.rect.height - 38) // 2
+      zone.draw(screen)
