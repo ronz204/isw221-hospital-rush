4a9235206f3bc7459e4f6c03afd2e986ee68952e
roney ruiz <roneyruizrojas@gmail.com>
2025-08-03 20:29:45 -0600

    feat: create validation for draggable objects

--- src/Components/DragDrop/DropZone.py
+++ src/Components/DragDrop/DropZone.py
@@ -1,17 +1,23 @@
 import pygame
-from typing import List
+from enum import Enum
+from typing import List, Tuple
 from pygame.event import Event
 from src.Models.Size import Size
 from pygame import draw, Surface
 from src.Components.Component import Component
 from src.Components.DragDrop.Draggable import Draggable
 
+class DropZoneState(Enum):
+  EMPTY = (120, 120, 120)
+  VALID = (100, 200, 100)
+  INVALID = (200, 100, 100)
+
 class DropZone(Component):
-  def __init__(self, coords, size: Size = Size(38, 38), padding: int = 10):
+  def __init__(self, coords, accepted_type: Draggable, size: Size = Size(38, 38)):
     super().__init__(coords=coords, size=size)
 
-    self.padding = padding
     self.object_inside: Component = None
+    self.accepted_type: Draggable = accepted_type
 
   @property
   def position(self):
@@ -22,7 +28,19 @@ class DropZone(Component):
   
   def center_drag(self, component: Component):
     component.rect.center = self.rect.center
+
+  def is_valid_object(self, draggable: Draggable) -> bool:
+    if self.accepted_type is None: return True
+    return isinstance(draggable, self.accepted_type)
   
+  def get_border_color(self) -> Tuple[int, int, int]:
+    if not self.object_inside:
+      return DropZoneState.EMPTY.value
+    
+    if self.is_valid_object(self.object_inside):
+      return DropZoneState.VALID.value
+    return DropZoneState.INVALID.value
+
   def update(self) -> None:
     if (self.object_inside):
       if not self.is_inside(self.object_inside):
@@ -39,5 +57,5 @@ class DropZone(Component):
           break
 
   def draw(self, screen: Surface) -> None:
-    border_color = (100, 200, 100) if self.object_inside else (120, 120, 120)
+    border_color = self.get_border_color()
     draw.rect(screen, border_color, self.rect, width=3, border_radius=6)
--- src/Components/Equipment/Stretcher.py
+++ src/Components/Equipment/Stretcher.py
@@ -6,6 +6,8 @@ from src.Helpers.AssetHelper import AssetHelper
 from src.Components.DragDrop.DropZone import DropZone
 from src.Components.DragDrop.Draggable import Draggable
 from src.Components.Equipment.BaseEquipment import BaseEquipment
+from src.Components.Characters.DoctorCharacter import DoctorCharacter
+from src.Components.Characters.PatientCharacter import PatientCharacter
 
 class Stretcher(BaseEquipment):
   def __init__(self, coords: Coord):
@@ -16,8 +18,8 @@ class Stretcher(BaseEquipment):
     stretcher_dropzone_coords = Coord(self.rect.x + (self.rect.width - 38) // 2, self.rect.y + (self.rect.height - 38) // 2)
 
     self.dropzones = [
-      DropZone(coords=doctor_dropzone_coords),
-      DropZone(coords=stretcher_dropzone_coords)]
+      DropZone(coords=doctor_dropzone_coords, accepted_type=DoctorCharacter),
+      DropZone(coords=stretcher_dropzone_coords, accepted_type=PatientCharacter),]
 
   def listen(self, event, draggables: List[Draggable]) -> None:
     for zone in self.dropzones:
