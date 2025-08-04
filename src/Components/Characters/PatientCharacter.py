8172adc950d52616defab1c4d9897616118f0439
roney ruiz <roneyruizrojas@gmail.com>
2025-08-04 08:35:31 -0600

    feat: cretae severity indicator for patients

--- src/Components/Characters/PatientCharacter.py
+++ src/Components/Characters/PatientCharacter.py
@@ -1,13 +1,33 @@
+import random
+from enum import Enum
+from pygame import mouse
 from src.Models.Size import Size
 from src.Models.Coord import Coord
-from src.Constants.Assets import Character
+from src.Helpers.AssetHelper import AssetHelper
+from src.Constants.Assets import Character, Font
 from src.Components.DragDrop.Draggable import Draggable
 from src.Components.Characters.BaseCharacter import BaseCharacter
+
+class PatientSeverity(Enum):
+  LOW = "Baja"
+  MEDIUM = "Media"
+  HIGH = "Alta"
 
 class PatientCharacter(BaseCharacter, Draggable):
   def __init__(self, coords: Coord, image: Character):
     super().__init__(coords=coords, size=Size(45, 55), image=image)
     Draggable.__init__(self)
 
+    self.hovered: bool = False
+    self.severity = random.choice(list(PatientSeverity))
+
   def listen(self, event) -> None:
     self.listen_drag(event)
+
+    mouse_pos = mouse.get_pos()
+    self.hovered = self.rect.collidepoint(mouse_pos)
+
+  def draw(self, screen) -> None:
+    if self.hovered:
+      severity_surf = AssetHelper.load_font(Font.KARMATIC.value, 18, f"Severidad {self.severity.value}")
+      screen.blit(severity_surf, (10, 30))
